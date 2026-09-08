71be9bc288b464b7489014ebcdced574431641c6
S.rahman <standerstate098@gmail.com>
2026-09-08 11:03:38 +0600

    #1 Tutorial -- select queries

---
+++ 01_SELECT_queries/main.sql
@@ -0,0 +1,23 @@
+-- https://sqlbolt.com/lesson/select_queries_introduction 
+-- Exercise 1 Tasks 
+  
+--1.Find the title of each film 
+
+SELECT title FROM movies ;
+    
+-- 2. Find the director of each film 
+
+SELECT directory FROM movies; 
+
+-- 3. Find the title and director of each film 
+
+SELECT title,director FROM movies; 
+
+-- 4. Find The title and year of each film 
+
+SELECT title,year FROM movies; 
+
+-- 5. Find all the information about each film 
+
+SELECT * FROM movies; 
+
